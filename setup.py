3d0cafca448035ae5bc99c92c95adf4a06136a5e
Anuroopgowda <anuroopgowdac@gmail.com>
2025-08-07 18:16:23 +0530

    init setup ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anuroop gowda c",
+    author_email="anuroopgowdac@gmail.com",
+    packages=find_packages()
+)
